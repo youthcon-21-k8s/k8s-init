3b93cf0e8be3a6ce32766e6006842cb1ff2ab7fe
GNU Shim <geunwoo.j.shim@gmail.com>
2021-12-08 22:28:55 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -41,9 +41,9 @@ modprobe br_netfilter
 sysctl --system
 
 # local small dns & vagrant cannot parse and delivery shell code.
-echo "192.168.1.10 m-k8s-y" >> /etc/hosts
-for (( i=1; i<=$1; i++  )); do echo "192.168.1.10$i w$i-k8s-y" >> /etc/hosts; done
-for (( i=1; i<=$1; i++  )); do echo "192.168.1.11$i s$i-k8s-y" >> /etc/hosts; done
+echo "192.168.56.10 m-k8s-y" >> /etc/hosts
+for (( i=1; i<=$1; i++  )); do echo "192.168.56.10$i w$i-k8s-y" >> /etc/hosts; done
+for (( i=1; i<=$1; i++  )); do echo "192.168.56.11$i s$i-k8s-y" >> /etc/hosts; done
 
 # config DNS  
 cat <<EOF > /etc/resolv.conf
