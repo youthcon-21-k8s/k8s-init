3b22c94c04728852dcb4962f47f89ff01742dfe7
GNU Shim <geunwoo.j.shim@gmail.com>
2021-12-08 22:29:54 +0900

    Update work_nodes.sh

--- work_nodes.sh
+++ work_nodes.sh
@@ -2,4 +2,4 @@
 
 # config for work_nodes only 
 kubeadm join --token 123456.1234567890123456 \
-             --discovery-token-unsafe-skip-ca-verification 192.168.1.10:6443
+             --discovery-token-unsafe-skip-ca-verification 192.168.56.10:6443
